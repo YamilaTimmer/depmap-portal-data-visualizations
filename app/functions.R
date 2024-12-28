a35039c5ec5c291050a823dde380a4894152c394
Yamila Timmer <y.timmer.2@st.hanze.nl>
2024-12-28 15:40:39 +0100

    Changed the way the figures get generated (seperate output for each type of plot), this was necessary because of the changes in UI (no longer a 'select graph type' button, but now using tab sets) and removed redundant code

--- app/functions.R
+++ app/functions.R
@@ -1,10 +1,25 @@
-# Read data
+# Load data
 load("C:\\Users\\yamil\\OneDrive - Hanze\\Bio-informatica\\Jaar 2\\2.1 applicatie\\tidy_expression.rdata")
 load("C:\\Users\\yamil\\OneDrive - Hanze\\Bio-informatica\\Jaar 2\\2.1 applicatie\\model.rdata")
 
-# Function for rendering barchart that shows gene expression per cell line (tab 1)
-generate_plot <- function(data, fill){
+
+#' Generate bar plot
+#'
+#' This function generates a bar plot using the merged data (tidy_expression + model)
+#'
+#' The generated bar plot shows the expression levels on the x-axis and the cell line name
+#' on the y-axis.
+#' 
+#' @param data merged dataframe of tidy_expression and model
+#' @param fill a column of the merged dataframe, depending on the filter options chosen by user
+#' @return a ggplot2 bar plot object
+#' @examples
+#' generate_barplot(merged, merged$AgeCategory)
+
+generate_barplot <- function(data, fill, fill_label){
+  
   # Generate barplot for when multiple genes are chosen
+  
   if (length(unique(data$gene)) > 1) {
     ggplot(data = data, 
            aes(x = expression, 
@@ -12,61 +27,103 @@ generate_plot <- function(data, fill){
                fill = fill)) +
       geom_bar(stat = "identity") + 
       ylab("Tumor Cell Line") +
-      xlab("Expression level(log2 TPM)") +
-      theme_minimal() + facet_wrap(~gene)
+      xlab("Expression level (log2 TPM)") +
+      labs(fill = fill_label) +
+      theme_minimal() + 
+      facet_wrap( ~ gene) # To display multiple bar plots of each of the chosen genes
   }
   
   # Generate barplot for when only one gene is chosen
+  
   else {
     ggplot(data = data, 
            aes(x = expression, 
-               y = reorder(StrippedCellLineName, expression))) + # Sort from high to low expression 
-      geom_bar(stat = "identity", fill = 'blue') + 
+               y = reorder(StrippedCellLineName, expression), # Sort from high to low expression 
+               fill = fill)) + 
+      geom_bar(stat = "identity") + 
       ylab("Tumor Cell Line") +
-      xlab("Expression level(log2 TPM)") +
+      xlab("Expression level (log2 TPM)") +
+      labs(fill = fill_label) +
       theme_minimal()
   }
 }
 
 
+#' Generate box plot
+#'
+#' This function generates a box plot using the merged data (tidy_expression + model)
+#'
+#' The generated box plot shows the gene name on the x-axis and the expression levels
+#' on the y-axis.
+#' 
+#' @param data merged dataframe of tidy_expression and model
+#' @return a ggplot2 box plot object
+#' @examples
+#' generate_box_plot(merged)
+
 generate_box_plot <- function(data){
   
   # Generate boxplot for when multiple genes are chosen
+  
   if (length(unique(data$gene)) > 1) {
     ggplot(data = data,
-           aes(x = gene, y = expression)) +
+           aes(x = gene, 
+               y = expression)) +
       geom_boxplot() +
-      labs(x = "Gene", y = "Gene Expression") + 
+      labs(x = "Gene", 
+           y = "Expression level(log2 TPM)") + 
       theme_minimal() + 
       theme(axis.text.x = element_text(angle = -90)) # Rotate gene names x-axis
   }
   
   # Generate boxplot for when only one gene is chosen
+  
   else {
     ggplot(data = data,
            aes(x = gene, 
                y = expression)) +
       geom_boxplot() +
-      labs(x = "Gene", y = "Gene Expression") + 
+      labs(x = "Gene", 
+           y = "Expression level(log2 TPM)") + 
       theme_minimal()
   }
 }
 
-generate_heatmap <- function(data){
-  if (length(unique(data$gene)) > 6) {
-    # Generate barplot for when multiple genes are chosen
-    ggplot(data = data, 
-           aes(x = gene, 
-               y = StrippedCellLineName, 
-               fill= expression)) +
-      geom_tile() + 
-      ylab("Tumor Cell Line") +
-      xlab("Gene") +
-      labs(fill = "Expression level(log2 TPM)") +
-      theme_minimal() + 
-      theme(axis.text.x = element_text(angle = -90))
-  }
-  else {
+
+
+#' Generate heat map
+#'
+#' This function generates a heat map using the merged data (tidy_expression + model)
+#' 
+#' The generated heat map shows the gene name on the x-axis, the cell line name on the
+#' y-axis and shows the expression levels with colour (fill).
+#' 
+#' @param data merged dataframe of tidy_expression and model
+#' @return a ggplot2 heat map object
+#' @examples
+#' generate_heatmap(merged)
+
+generate_heatmap <- function(data, text_angle){
+  
+  # Generate heat map for when less than 6 genes are selected
+  
+  # if (length(unique(data$gene)) < 6) {
+  #   ggplot(data = data, 
+  #          aes(x = gene, 
+  #              y = StrippedCellLineName, 
+  #              fill= expression)) +
+  #     geom_tile() + 
+  #     ylab("Tumor Cell Line") +
+  #     xlab("Gene") +
+  #     labs(fill = "Expression level (log2 TPM)") +
+  #     theme_minimal() + 
+  #     scale_fill_distiller(palette = "RdPu")
+  #   
+  # }
+  # 
+  # else {
+    
+    # Generate heat map for when more than 6 genes are selected (rotates x-axis labels)
     
     ggplot(data = data, 
            aes(x = gene, 
@@ -75,10 +132,11 @@ generate_heatmap <- function(data){
       geom_tile() + 
       ylab("Tumor Cell Line") +
       xlab("Gene") +
-      labs(fill = "Expression level(log2 TPM)") +
-      theme_minimal() + scale_fill_distiller(palette = "RdPu")
+      labs(fill = "Expression level (log2 TPM)") +
+      theme_minimal() + 
+      theme(axis.text.x = element_text(angle = text_angle))
   }
-}
+
 
 
 
@@ -89,16 +147,16 @@ generate_table <- function(data){
                   matches("expression"))
 }
 
-
-merge_data <- function(filtered_metadata, filtered_expr) {
-  
-  filtered_metadata <- filter_data(input)
-  filtered_expr <- filter_expression(filtered_metadata, input)
-  merged <- merge(filtered_metadata[, c("ModelID", "StrippedCellLineName")], filtered_expr, by = "ModelID", all = FALSE)
-  
-  
-  return(merged)
-}
+# 
+# merge_data <- function(filtered_metadata, filtered_expr) {
+#   
+#   filtered_metadata <- filter_data(input)
+#   filtered_expr <- filter_expression(filtered_metadata, input)
+#   merged <- merge(filtered_metadata[, c("ModelID", "StrippedCellLineName")], filtered_expr, by = "ModelID", all = FALSE)
+#   
+#   
+#   return(merged)
+# }
 
 
 # # Function for rendering barchart that shows gene expression of one gene across multiple cell lines (tab 3)
--- app/server.R
+++ app/server.R
@@ -35,70 +35,6 @@ server <- function(input, output, session) {
                   selected = sort(model$StrippedCellLineName[1]))
   
   
-  
-  
-  # Filters data based on user input
-  # filter_data <- function(input){
-  #   
-  #   filtered_metadata <- model %>% 
-  #     filter(Sex %in% input$sex 
-  #            & PatientRace %in% input$race 
-  #            & AgeCategory %in% input$age_category 
-  #            & OncotreePrimaryDisease %in% input$onco_type)
-  #   
-  #   
-  #   # Uses the input of the slider to decide how many cell lines will be displayed
-  #   #filtered <- head(filtered, input$cell_line_number)
-  # 
-  #   # If checkbox is checked, expression values of 0 will not be displayed
-  #   #if(input$checkbox == TRUE) 
-  #   
-  #   #filtered <- filtered %>%
-  #       #filter(expression != 0)
-  #   
-  #   return(filtered_metadata)
-  #   
-  # }
-  # 
-  # 
-  # filter_expression <- function(filtered_metadata, input) {
-  #   filtered_expr <- tidy_expression %>%
-  #     filter(
-  #       X %in% filtered_metadata$ModelID,  # Match with ModelID or equivalent identifier
-  #       gene %in% input$gene_name          # Filter based on selected genes
-  #     )
-  #   
-  #   return(filtered_expr)
-  # }
-  # 
-  # # # Renders barchart that shows gene expression per cell line (tab 1)
-  # # output$plot_per_cell_line <- renderPlotly({
-  # #   
-  # #   filtered <- filter_data(tidy_merged, input)
-  # #   filtered_per_cell_line <- filtered %>% filter(StrippedCellLineName == 
-  # #                                                   input$cell_line)
-  # #   
-  # #   plot_per_cell_line <- generate_plot_per_cell_line(filtered_per_cell_line)
-  # #   return(plot_per_cell_line)
-  # #   
-  # #   
-  # # })
-  # 
-  
-  
-  # # Renders barchart that shows gene expression of one gene across multiple cell lines (tab 3)
-  # output$plot_per_gene <- renderPlotly({
-  #   
-  #   filter_data(input)
-  #   filter_expression(filtered_metadata, input)
-  # 
-  #   plot_per_gene <- generate_plot(filtered_expr)
-  #   return(plot_per_gene)
-  #   
-  #   
-  # })
-  
-  
   # Function to filter metadata based on input values
   filter_data <- function(input) {
     filtered_metadata <- model %>% 
@@ -126,59 +62,72 @@ server <- function(input, output, session) {
     
     filtered_metadata <- filter_data(input)
     filtered_expr <- filter_expression(filtered_metadata, input)
-    merged <- merge(filtered_metadata[, c("ModelID", "StrippedCellLineName", "Sex", "PatientRace", "AgeCategory")], filtered_expr, by = "ModelID", all = FALSE)
+    merged <- merge(filtered_metadata[, c("ModelID", "StrippedCellLineName", "Sex", "PatientRace", "AgeCategory", "OncotreePrimaryDisease")], filtered_expr, by = "ModelID", all = FALSE)
     
     
     return(merged)
   }
   
   
+  output$boxplot_per_gene <- renderPlotly({
+    merged <- merge_data(filtered_metadata, filtered_expr)
+    boxplot_per_gene <- generate_box_plot(merged)
+    return(boxplot_per_gene)
+  })
   
-  # Render Plotly plot
-  output$plot_per_gene <- renderPlotly({
+  output$heatmap_per_gene <- renderPlotly({
     merged <- merge_data(filtered_metadata, filtered_expr)
     
-    if (input$plot_type == "Barchart") {
-      if (input$visualise_parameter == "Sex") {
-        
-        plot_per_gene <- generate_plot(merged, merged$Sex)
-        
-      }
+    if (length(unique(merged$gene)) > 6) {
       
-      if (input$visualise_parameter == "Race") {
-        
-        plot_per_gene <- generate_plot(merged, merged$PatientRace)
-        
-      }
-      
-      if (input$visualise_parameter == "Age Category") {
-        
-        plot_per_gene <- generate_plot(merged, merged$AgeCategory)
-        
-      }
-      # Step 3: Generate plot using the filtered expression data
-      return(plot_per_gene)
+      text_angle = -90
+    
     }
-    
-    
-    if (input$plot_type == "Heatmap") {
-      
-      heatmap_plot <- generate_heatmap(merged)
-      return(heatmap_plot)
-    }
-    
     else {
-      boxplot_per_gene <- generate_box_plot(merged)
-      return(boxplot_per_gene)
+      text_angle = 0
     }
+    heatmap_per_gene <- generate_heatmap(merged, text_angle)
+    return(heatmap_per_gene)
+  })
+  
+  output$barplot_per_gene <- renderPlotly({
+    merged <- merge_data(filtered_metadata, filtered_expr)
+    
+    if (input$barplot_parameter == "Sex") {
+      
+      barplot_per_gene <- generate_barplot(merged, merged$Sex, "Sex")
+      
+    }
+    
+    if (input$barplot_parameter == "Race") {
+      
+      barplot_per_gene <- generate_barplot(merged, merged$PatientRace, "Race")
+      
+    }
+    
+    if (input$barplot_parameter == "Age Category") {
+      
+      barplot_per_gene <- generate_barplot(merged, merged$AgeCategory, "Age Category")
+ 
+    }
+    
+    if (input$barplot_parameter == "Cancer Type") {
+      
+      barplot_per_gene <- generate_barplot(merged, merged$OncotreePrimaryDisease, "Cancer Type")
+      
+    }
+    
+    
+    return(barplot_per_gene)
   })
   
   
   # Renders table with filtered data (tab 2)
-  output$table <- renderDT({
+  output$filtered_table <- renderDT({
     merged <- merge_data(filtered_metadata, filtered_expr)
     generate_table(merged)
   })
+  
   
   
   # Allows for downloading data as .csv file
@@ -189,6 +138,7 @@ server <- function(input, output, session) {
     contentType = "text/csv",
     content = function(file) {
       filtered <- filter_data(input)
+      
       # FALSE for row.names so no 'empty' column will be made with indexes
       write.csv(filtered, file, row.names = FALSE)
     }
